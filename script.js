da5693e45fcdfdd0b86ca402c9acfa9fe81ada2f
Aldyza1425 <161410869+Aldyza1425@users.noreply.github.com>
2024-03-10 12:57:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    document.querySelector(".loader").style.opacity = 0;
+    setTimeout(() => {
+        document.querySelector(".loader").style.display = "none";
+    }, 1000);
+}
+
